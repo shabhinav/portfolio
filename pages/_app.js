54b30c98243e0a13c776f09e38bc79eae677230b
abhinav44 <abhinav@quoality.com>
2022-06-12 21:53:41 +0530

    ui changes

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ import "../styles/globals.css";
 import "../styles/themes.css";
 
 function MyApp({ Component, pageProps }) {
-
   useEffect(() => {
     if (localStorage.getItem("theme")) {
       document.documentElement.setAttribute(
@@ -18,6 +17,18 @@ function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Head title={`Abhinav Sharma | ${pageProps.title}`} />
+      <link
+        rel='stylesheet'
+        type='text/css'
+        charset='UTF-8'
+        href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css'
+      />
+      <link
+        rel='stylesheet'
+        type='text/css'
+        href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css'
+      />
+
       <Component {...pageProps} />
     </Layout>
   );
