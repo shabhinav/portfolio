644ec6a1bcca637648dcab994ed0db6cdfc4cceb
Abhinav Sharma <abhinavsharma@Abhinavs-MacBook-Air.local>
2024-01-29 22:36:40 +0530

    project reordering and new  about

--- pages/about.jsx
+++ pages/about.jsx
@@ -2,28 +2,23 @@ const AboutPage = () => {
   return (
     <>
       <h3>
-        Hi, my name is Abhinav. I am a Frontend Web developer and aspiring Full
-        Stack developer and also Javascript enthusiast.
+        Hey there! I'm Abhinav, a Frontend Web Developer who's also into becoming a Full Stack Developer.I really enjoy working with JavaScript and its cool frameworks like React.js, React Native and Next.js.
       </h3>
       <br />
-      <h3>My primary focus is on building Frontend web applications.</h3>
+      <h3>I'm good at languages like JavaScript, TypeScript, CSS3, and HTML5. I use tools like Redux, Bootstrap, Apollo-Graphql, and Firebase to make websites look and work great. I'm also familiar with Webpack and RollUp, which help make the development process smoother.</h3>
       <br />
 
       <h3>
-        My skillsets include languages like JavaScript,TypeScript ,CSS3, HTML5, SCSS/SASS
-        together with frameworks like React.js,Next.js,React-Native Redux,
-        Bootstrap,,Apollo-Graphql,Webpack?RollUp,Firebase,Tailwind,StoryBook.
+        I've worked as a Frontend Developer at Trademo and was a Lead Frontend Developer at Quoality before that. I love creating awesome user interfaces that make websites fun to use.
       </h3>
       <br />
 
       <h3>
-        I do this because I love the process of turning dreams into reality by
-        converting ideas into code. I have a serious passion for designing
-        top-grade UI's with working logic to create a dynamic user experience.
+        Outside of coding, I'm into playing football (soccer), learning about new tech stuff, and listening to audiobooks and podcasts. Let's chat and share our love for frontend development!
       </h3>
       <br />
 
-      <h3>
+      {/* <h3>
         I'm working as a Frontend Developer in Trademo and worked as a
         Lead Frontend Developer at Quoality.
       </h3>
@@ -31,7 +26,7 @@ const AboutPage = () => {
       <h3>
         On a Side note, I love playing Football(Soccer), learning new
         technologies and listening Audiobooks and Podcast.
-      </h3>
+      </h3> */}
     </>
   );
 };
@@ -43,3 +38,11 @@ export async function getStaticProps() {
 }
 
 export default AboutPage;
+
+
+
+
+
+
+
+
